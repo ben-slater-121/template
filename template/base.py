5ea047e5de6f67a7106b827d8f05d3b1e414b1d0
ben-slater-121 <ben-slater-121@users.noreply.github.com>
2021-10-08 09:05:12 +0000

    ✅ Ready to clone and code.

--- template/base.py
+++ template/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+template base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the template project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
